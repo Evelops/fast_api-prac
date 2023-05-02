55a8dd182f1d9facffd26e06556473483927c646
eomseung-yeol <co6747@naver.com>
2023-05-02 19:47:18 +0900

    Feat: 사전 학습된 모델기반(w2v) 추천 api 테스트

--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 from typing import Union
+from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, JSONResponse
+from gensim.models import KeyedVectors
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_root():
+    # 모델 로드
+    loaded_model = KeyedVectors.load_word2vec_format("model/news_w2v_2")
+    # 키워드 기반 추천
+    model_result = loaded_model.most_similar("한국")
+    # 추천 리스트
+    recList = []
+    for res in model_result:
+        word, score = res
+        recList.append({'word': word, 'score': score})
+    return recList
